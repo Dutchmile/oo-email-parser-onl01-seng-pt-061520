45cdba497448682875502405322786c6c4cb7c51
Christopher Dutcher <cadutcher@icloud.com>
2020-07-29 21:34:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,9 +4,20 @@
 # or whitespace (' ').
 class EmailAddressParser
 
-  def initialize(new)
-    self.split(" ")
-  end
+  attr_accessor :name, :csv_emails
+  
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
+  end 
+
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
+
 
 
 
